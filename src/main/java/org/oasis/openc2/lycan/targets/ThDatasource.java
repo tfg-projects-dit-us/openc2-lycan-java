f6dcefbf5755f1927e93ada06f3dee0a15f20aab
MIT-FS <irm@us.es>
2026-09-21 19:52:32 +0200

    añadiendo target thdatasource

---
+++ src/main/java/org/oasis/openc2/lycan/targets/ThDatasource.java
@@ -0,0 +1,11 @@
+package org.oasis.openc2.lycan.targets;
+
+public class ThDatasource {
+    private String name;
+
+    public ThDatasource() {}
+    public ThDatasource(String name) { this.name = name; }
+
+    public String getName() { return name; }
+    public void setName(String name) { this.name = name; }
+}
